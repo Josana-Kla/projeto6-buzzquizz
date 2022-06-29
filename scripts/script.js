07a07421bd697da379b54f31b55051f94f5b9549
GustavoHartmann <gustavolerner.hartmann@gmail.com>
2022-06-29 20:24:13 -0300

    adicionei a página da criação de perguntas do quizz

--- scripts/script.js
+++ scripts/script.js
@@ -5,7 +5,52 @@ let pegarClasseConteudoNoHtml = document.querySelector('.conteudo');
 /* ------------------------- FUNÇÕES DE CRIAÇÃO ------------------------- */
 
 function criarQuizz () {
-    
+    pegarClasseConteudoNoHtml.innerHTML = `
+    <div class="pag-criacao-quizz">
+            <h2 class="titulo-instrucao">Comece pelo começo</h2>
+            <div class="caixa-especificacoes">
+                <input type="text" name="tituloQuizz" placeholder="Título do seu quizz">
+                <input type="text" name="URLQuizz" placeholder="URL da imagem do seu quizz">
+                <input type="text" name="qtdPerg" placeholder="Quantidade de perguntas do seu quizz">
+                <input type="text" name="qtdNiveis" placeholder="Quantidade de níveis do seu quizz">
+            </div>
+            <div class="botao criar-perguntas" onclick="prosseguirParaPerguntas()">
+                <h2>Prosseguir para criar perguntas</h2>
+            </div>
+        </div>
+    `
+}
+
+function prosseguirParaPerguntas () {
+    pegarClasseConteudoNoHtml.innerHTML = `
+    <div class="pag-criacao-perguntas">
+            <h2 class="titulo-instrucao">Crie suas perguntas</h2>
+            <div class="caixa-perguntas">
+                <div class="numero-pergunta">
+                    <h2 class="subtitulo-instrucao">Pergunta 1</h2>
+                    <input type="text" name="pergunta" placeholder="Texto da pergunta">
+                    <input type="text" name="corPergunta" placeholder="Cor de fundo da pergunta">
+                </div>
+                <div class="resposta-correta">
+                    <h2 class="subtitulo-instrucao">Resposta correta</h2>
+                    <input type="text" name="resposta" placeholder="Resposta correta">
+                    <input type="text" name="URLResposta" placeholder="URL da imagem">
+                </div>
+                <div class="respostas-incorretas">
+                    <h2 class="subtitulo-instrucao">Respostas incorretas</h2>
+                    <input type="text" name="respostaIncorreta1" placeholder="Resposta incorreta 1">
+                    <input type="text" name="URLRespostaIncorreta1" placeholder="URL da imagem 1">
+                    <input type="text" name="respostaIncorreta2" placeholder="Resposta incorreta 2">
+                    <input type="text" name="URLRespostaIncorreta2" placeholder="URL da imagem 2">
+                    <input type="text" name="respostaIncorreta3" placeholder="Resposta incorreta 3">
+                    <input type="text" name="URLRespostaIncorreta3" placeholder="URL da imagem 3">
+                </div>
+            </div>
+            <div class="botao criar-niveis">
+                <h2>Prosseguir para criar níveis</h2>
+            </div>
+        </div>
+    `
 }
 
 
@@ -64,7 +109,7 @@ if(id !== idsUsuario) { "roda a funcao quizzesDeOutrosUsuarios" } else if(id ===
 
 function quizzesDeOutrosUsuarios() {
 
-    pegarClasseConteudoNoHtml.innerHTML = `
+    pegarClasseConteudoNoHtml.innerHTML += `
         <div class="todos-quizzes">
             <h2>Todos os Quizzes</h2>
             <div class="lista-todos-quizzes">
